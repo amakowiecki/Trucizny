044c7dbbfb58ee465ad09d436a2a64bba6e1c692
Adrian Makowiecki <mcowiecki@gmail.com>
2015-12-04 15:05:59 +0100

    Model ocen produktów

---
+++ Sklep z truciznami/Models/Rating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sklep_z_truciznami.Models
+{
+    public class Rating
+    {
+        [Required]
+        public string ClientId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Rate { get; set; }
+
+    }
+}
