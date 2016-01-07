2b37770883da0e6cc33ec8ed7e1b7b89ca4cb308
sebaku10 <sebastian.kutyla@o2.pl>
2016-01-07 22:21:28 +0100

    Widok edycji danych użytkownika: - poprawić wyświetlanie etykiet (dodać [Display(Name="")] do pól w modelu) - poprawić nazwy przycisków na polskie przy zapisywaniu danych i zmianie hasła

--- Sklep z truciznami/Models/AdressDataModel.cs
+++ Sklep z truciznami/Models/AdressDataModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,19 @@ namespace Sklep_z_truciznami.Models
 {
     public class AdressDataModel
     {
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
+
+        [Display(Name = "Ulica i numer domu")]
         public string StreetAndNumber { get; set; }
+
+        [Display(Name = "Miejscowość")]
         public string City { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
         public string ZipCode { get; set; }
     }
 }
